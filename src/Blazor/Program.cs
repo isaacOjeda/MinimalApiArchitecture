be6a0cc1a6a94aba774ec004d686720b6b0dfa52
Isaac Ojeda <isaac.ojeda@outlook.com>
2021-11-07 11:28:50 -0700

    Blazor Client

---
+++ src/Blazor/Program.cs
@@ -0,0 +1,18 @@
+using Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp =>
+{
+    var host = builder.Configuration["Host"];
+    var httpClient = sp.GetRequiredService<HttpClient>();
+
+    return new ApiClient.Client(host, httpClient);
+});
+
+await builder.Build().RunAsync();
