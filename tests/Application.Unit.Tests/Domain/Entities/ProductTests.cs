75b2aa205f609e5187d34bdd4578cf5244e5b2c8
Isaac Ojeda <isaac.ojeda@intelectix.com>
2021-11-24 21:26:04 -0700

    Added domain model tests

---
+++ tests/Application.Unit.Tests/Domain/Entities/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using MinimalApiArchitecture.Application.Domain.Entities;
+using MinimalApiArchitecture.Application.Features.Products.Commands;
+using NUnit.Framework;
+
+namespace Application.Unit.Tests.Domain.Entities
+{
+    public class ProductTests
+    {
+        [TestCase(1, 0)]
+        [TestCase(999, 1)]
+        public void ProductPriceChanged(double price, int eventsCount)
+        {
+            // Arrenge
+            var product = new Product(1, "Name 1", "Description 1", 1, 1);
+            var command = new UpdateProduct.UpdateProductCommand
+            {
+                CategoryId = 1,
+                Description = "New description",
+                Name = "New name",
+                Price = price
+            };
+
+            // Act
+            product.UpdateInfo(command);
+
+            // Assert
+            product.Price.Should().Be(price);
+            product.DomainEvents.Count.Should().Be(eventsCount);
+        }
+    }
+}
